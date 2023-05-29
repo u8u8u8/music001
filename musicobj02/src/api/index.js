6c0831a16d1c9d40dbae816a8a0287e85be39463
zhuyq <ziyoubacao@qq.com>
2023-05-29 16:32:08 +0800

    2023-5-29

--- musicobj02/src/api/index.js
+++ musicobj02/src/api/index.js
@@ -7,9 +7,14 @@ export function getBanner(type=0){
     return axios.get(`${baseUrl}/banner?type=${type}`);
 
 }
-//封装获取轮播图api   调用例子：/banner,/banner?type=2 type=0,1,2,3
+//封装音乐列表api   调用例子：/personalized?limit=30 limit=10,50,100
 export function getMusic(limit=10){
     return axios.get(`${baseUrl}/personalized?limit=${limit}`);
+
+}
+//封装歌单详情的api   调用例子：/playlist/detail?id=24381616
+export function getMusicList(id){
+    return axios.get(`${baseUrl}/playlist/detail?id=${id}`);
 
 }
 //对外抛出
